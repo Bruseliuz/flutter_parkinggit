30f4f94b64372bdd7ad2c9ee12d26231239eee92
mJarsater <martin.jarsater@hotmail.com>
2020-04-08 11:09:36 +0200

    Sign In Page

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutterparkinggit/gamla_appen/models/user.dart';
-import 'package:flutterparkinggit/gamla_appen/services//pages/wrapper.dart';
-import 'package:flutterparkinggit/gamla_appen/services/auth.dart';
-import 'package:flutterparkinggit/gamla_appen/services//pages/homescreens/home.dart';
-import 'package:flutterparkinggit/gamla_appen/services//pages/map.dart';
-import 'package:flutterparkinggit/gamla_appen/services/pages/authenticate/register.dart';
-import 'package:flutterparkinggit/gamla_appen/services/api_getter.dart';
-import 'package:provider/provider.dart';
+import 'package:flutterparkinggit/sign_in.dart';
 
 void main() => runApp(MyApp());
 
@@ -16,10 +9,10 @@ class MyApp extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
     return  MaterialApp(
-      initialRoute: "/home",
+      initialRoute: "/signIn",
         routes: {
+          '/signIn': (context) => SignIn(),
         },
-        home: Wrapper(),
-      );
+    );
   }
 }
