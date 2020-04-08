c26d45daf25264cbbfd7e9afdfaa3818a968407a
Rebecctor <th.rebecca@hotmail.com>
2020-04-08 11:48:04 +0200

    sign in

--- lib/sign_in.dart
+++ lib/sign_in.dart
@@ -7,13 +7,16 @@ class SignIn extends StatefulWidget {
 }
 
 class _SignInState extends State<SignIn> {
+  String _email, _password;
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(
-          'Sign in page'
+          'Sign in'
         ),
+
         actions: <Widget>[
           FlatButton.icon(
             onPressed: () {
@@ -21,11 +24,52 @@ class _SignInState extends State<SignIn> {
             },
             icon: Icon(Icons.settings),
             label: Text(
-              'Register'
+              'Register account'
             ),
           )
         ],
       ),
+      body: Form(
+        key: _formKey,
+        child: Column(
+          children: <Widget>[
+            TextFormField(
+              validator: (input) {
+                if(input.isEmpty) {
+                  return 'Can not be empty';
+                }
+              },
+              onSaved: (input) => _email = input,
+              decoration: InputDecoration(
+                labelText: 'Email'
+              ),
+            ),
+        TextFormField(
+            validator: (input) {
+              if(input.isEmpty) {
+                return 'Can not be empty';
+              }
+            },
+            onSaved: (input) => _password = input,
+            decoration: InputDecoration(
+              labelText: 'Password'
+            ),
+          obscureText: true,
+        ),
+            RaisedButton (
+              onPressed: () {},
+              child: Text('Sign in'),
+
+            )
+          ],
+        )
+      )
     );
   }
+
+  void signIn() {
+    //validate
+    //login
+}
+
 }
