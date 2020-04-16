98ea0f951ad3c80d8abb7f071e07b97713c236c4
mJarsater <martin.jarsater@hotmail.com>
2020-04-16 14:40:31 +0200

    Map - Hämta api beroende av position

--- lib/gamla_appen/services/pages/map/map.dart
+++ lib/gamla_appen/services/pages/map/map.dart
@@ -171,8 +171,9 @@ class _MapState extends State<Map> {
     print('1 check'); //
     Response response = await get('https://openparking.stockholm.se/LTF-Tolken/v1/ptillaten/within?radius=100&lat=${location.latitude.toString()}&lng=${location.longitude.toString()}&outputFormat=json&apiKey=e734eaa7-d9b5-422a-9521-844554d9965b');
     LinkedHashMap data = jsonDecode(response.body);
+    print(data['totalFeatures']);
     print('2 check');
-    print(data);
+
   }
     @override
     Widget build(BuildContext context) {
