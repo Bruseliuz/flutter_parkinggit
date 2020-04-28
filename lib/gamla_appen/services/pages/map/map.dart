a02c7550d33bac0afcd4400c5c8f2b44b4e939d8
mJarsater <martin.jarsater@hotmail.com>
2020-04-28 11:34:09 +0200

    Markers byter on location

--- lib/gamla_appen/services/pages/map/map.dart
+++ lib/gamla_appen/services/pages/map/map.dart
@@ -144,7 +144,6 @@ class _MapState extends State<Map> {
     final GoogleMapController controller = await _controller.future;
     controller.animateCamera(CameraUpdate.newCameraPosition(cameraPosition));
     getData(newLocation);
-    getMarkers(); // Sätter alla markers på kartan.
   }
 
   Future getMarkers() async{
@@ -186,6 +185,9 @@ class _MapState extends State<Map> {
         onPressed: () {
           allMarkers.clear();// Ta bort alla Markers ifrån kartan
           getCurrentLocation();
+          setState(() {
+            getMarkers();
+          });
         },
       ),
     );
