2e9c2448ca6eb97043d6a9f298034b4ca08d591c
bruseliuz <bruzeliusnils@gmail.com>
2020-05-07 21:04:08 +0200

    Nu fungerar det på iOS på min!

--- lib/gamla_appen/services/pages/map/map.dart
+++ lib/gamla_appen/services/pages/map/map.dart
@@ -1,6 +1,8 @@
-import 'dart:typed_data';
-import 'dart:ui';
 
+import 'dart:typed_data';
+import 'dart:io';
+import 'dart:ui' as ui;
+import 'dart:ui';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
