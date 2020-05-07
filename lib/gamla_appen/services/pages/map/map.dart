0a4bf5ffe9cfb9d045bfb2c7d20200e46bb39b09
Rebecctor <th.rebecca@hotmail.com>
2020-05-07 11:46:22 +0200

    markerkuk

--- lib/gamla_appen/services/pages/map/map.dart
+++ lib/gamla_appen/services/pages/map/map.dart
@@ -1,3 +1,6 @@
+import 'dart:typed_data';
+import 'dart:ui';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
@@ -229,11 +232,12 @@ class _ParkingMapState extends State<ParkingMap> {
   }
 
   void getMarkers() {
-    parkingSpotsList.forEach((element) {
+    parkingSpotsList.forEach((element) async {
+      BitmapDescriptor bitmapDescriptor = await createCustomMarkerBitmap(element.availableParkingSpots);
       setState(() {
         allMarkers.add(Marker(
             markerId: MarkerId(element.streetName),
-            icon: BitmapDescriptor.defaultMarker,
+            icon: bitmapDescriptor,
             visible: true,
             draggable: false,
             onTap: () {
@@ -245,6 +249,44 @@ class _ParkingMapState extends State<ParkingMap> {
       });
     });
   }
+
+  Future<BitmapDescriptor> createCustomMarkerBitmap(String title) async {
+    TextSpan span = new TextSpan(
+      style: new TextStyle(
+        color: Colors.black,
+        fontSize: 35.0,
+        fontWeight: FontWeight.bold,
+      ),
+      text: title,
+    );
+
+    TextPainter tp = new TextPainter(
+      text: span,
+      textAlign: TextAlign.center,
+      textDirection: TextDirection.ltr,
+    );
+
+    PictureRecorder recorder = new PictureRecorder();
+    Canvas c = new Canvas(recorder);
+
+    tp.layout();
+    tp.paint(c, new Offset(20.0, 10.0));
+
+    /* Do your painting of the custom icon here, including drawing text, shapes, etc. */
+
+    Picture p = recorder.endRecording();
+    ByteData pngBytes =
+    await (await p.toImage(tp.width.toInt() + 40, tp.height.toInt() + 20))
+        .toByteData(format: ImageByteFormat.png);
+
+    Uint8List data = Uint8List.view(pngBytes.buffer);
+
+    return BitmapDescriptor.fromBytes(data);
+  }
+
+
+
+
 
   void getFavorites() async {
     final QuerySnapshot result = await Firestore.instance
