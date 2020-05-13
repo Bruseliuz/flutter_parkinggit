542951dd765731c5c794690a244810f9efcfe596
mJarsater <martin.jarsater@hotmail.com>
2020-05-13 15:02:43 +0200

    Timer

--- lib/gamla_appen/services/pages/map/park_timer.dart
+++ lib/gamla_appen/services/pages/map/park_timer.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
@@ -17,11 +19,11 @@ class _ParkTimerState extends State<ParkTimer> {
   String setParkingText = '';
   Color startTimerColor = Colors.green[300];
   Color endTimerColor = Colors.red[100];
-  bool timerStarted;
+  bool timerStarted = false;
 
   @override
   Widget build(BuildContext context) {
-    if (picked == null) {
+    if(picked == null){
       picked = TimeOfDay.now();
     }
     return Scaffold(
@@ -122,16 +124,16 @@ class _ParkTimerState extends State<ParkTimer> {
                               fontWeight: FontWeight.w600
                           ),),
                         FloatingActionButton.extended(
-                          onPressed: () {
+                          onPressed: (){
                             selectTime(context);
                           },
                           icon: Icon(Icons.watch_later,
-                            color: Color(0xff207FC5),),
+                          color: Color(0xff207FC5),),
                           backgroundColor: Colors.white,
                           label: Text('SET TIMER',
-                            style: TextStyle(
-                                color: Color(0xff207FC5)
-                            ),),
+                          style: TextStyle(
+                            color: Color(0xff207FC5)
+                          ),),
                         )
                       ],
                     ),
@@ -164,42 +166,40 @@ class _ParkTimerState extends State<ParkTimer> {
                       padding: EdgeInsets.fromLTRB(0, 20, 0, 50),
                       child: Column(
                         children: <Widget>[
-                          SizedBox(width: 10),
-                          Text('REGISTRATION NUMBER',
-                            style: TextStyle(
-                              color: Colors.white,
-                              fontWeight: FontWeight.bold,
-                              fontSize: 12,
-                            ),
-                          ),
+                             SizedBox(width: 10),
+                             Text( 'REGISTRATION NUMBER',
+                               style: TextStyle(
+                                 color: Colors.white,
+                                 fontWeight: FontWeight.bold,
+                                 fontSize: 12,
+                               ),
+                             ),
                           SizedBox(height: 5.0),
                           Row(
                             children: <Widget>[
                               Container(
-                                child: TextFormField(
-                                  inputFormatters: [
-                                    LengthLimitingTextInputFormatter(3),
-                                  ],
-                                  decoration: InputDecoration(
-                                      hintText: 'ABC',
-                                      hintStyle: TextStyle(
+                                  child: TextFormField(
+                                    inputFormatters: [
+                                      LengthLimitingTextInputFormatter(3),
+                                    ],
+                                    decoration: InputDecoration(
+                                        hintText: 'ABC',
+                                        hintStyle: TextStyle(
                                           color: Color(0xff207FC5)
-                                      ),
-                                      border: InputBorder.none
+                                        ),
+                                        border: InputBorder.none
+                                    ),
+                                    keyboardType: TextInputType.text,
                                   ),
-                                  keyboardType: TextInputType.text,
-                                ),
                                 width: 70,
                                 decoration: BoxDecoration(
                                     color: Colors.white,
-                                    borderRadius: BorderRadius.only(
-                                        topLeft: Radius.circular(10),
-                                        bottomLeft: Radius.circular(10)),
+                                    borderRadius: BorderRadius.only(topLeft: Radius.circular(10),bottomLeft: Radius.circular(10)),
                                     boxShadow: [
                                       BoxShadow(
                                           color: Colors.black26,
                                           blurRadius: 5.0,
-                                          offset: Offset(0, 2)
+                                          offset: Offset(0,2)
                                       )
                                     ]
                                 ),
@@ -207,30 +207,28 @@ class _ParkTimerState extends State<ParkTimer> {
                               ),
                               SizedBox(width: 0.5),
                               Container(
-                                child: TextFormField(
-                                  inputFormatters: [
-                                    LengthLimitingTextInputFormatter(3),
-                                  ],
-                                  decoration: InputDecoration(
-                                      hintText: '123',
-                                      hintStyle: TextStyle(
-                                          color: Color(0xff207FC5)
-                                      ),
-                                      border: InputBorder.none
+                                  child: TextFormField(
+                                    inputFormatters: [
+                                      LengthLimitingTextInputFormatter(3),
+                                    ],
+                                    decoration: InputDecoration(
+                                        hintText: '123',
+                                        hintStyle: TextStyle(
+                                            color: Color(0xff207FC5)
+                                        ),
+                                        border: InputBorder.none
+                                    ),
+                                    keyboardType: TextInputType.number,
                                   ),
-                                  keyboardType: TextInputType.number,
-                                ),
                                 width: 70,
                                 decoration: BoxDecoration(
                                     color: Colors.white,
-                                    borderRadius: BorderRadius.only(
-                                        topRight: Radius.circular(10),
-                                        bottomRight: Radius.circular(10)),
+                                    borderRadius: BorderRadius.only(topRight: Radius.circular(10),bottomRight: Radius.circular(10)),
                                     boxShadow: [
                                       BoxShadow(
                                           color: Colors.black26,
                                           blurRadius: 5.0,
-                                          offset: Offset(3, 2)
+                                          offset: Offset(3,2)
                                       )
                                     ]
                                 ),
@@ -247,11 +245,15 @@ class _ParkTimerState extends State<ParkTimer> {
                             elevation: 4.0,
                             onPressed: () {
                               setState(() {
-                                startTimerColor = Colors.green[100];
-                                endTimerColor = Colors.red[300];
-                                timerStarted = true;
+                                if(picked == TimeOfDay.now()){
+                                  noTimeSelectedDialog(context);
+                                }else{
+                                  startTimerColor = Colors.lightGreen[100];
+                                  endTimerColor = Colors.red[300];
+                                  timerStarted = true;
+                                }
                               });
-                            },
+                              },
                             padding: EdgeInsets.all(15),
                             child: Row(
                               mainAxisAlignment: MainAxisAlignment.center,
@@ -279,7 +281,9 @@ class _ParkTimerState extends State<ParkTimer> {
                             color: endTimerColor,
                             elevation: 4.0,
                             onPressed: () {
-                              cancelAlertDialog(context);
+                              if(timerStarted == true){
+                                Navigator.pop(context);
+                              }
                             },
                             padding: EdgeInsets.all(15),
                             child: Row(
@@ -315,24 +319,39 @@ class _ParkTimerState extends State<ParkTimer> {
   }
 
 
-  Future<void> cancelAlertDialog(BuildContext context) {
+  Future<void> noTimeSelectedDialog(BuildContext context) {
     return showDialog<void>(
       context: context,
       builder: (BuildContext context) {
-        return AlertDialog(
-          title: Text('Sure?'),
-          actions: <Widget>[
-            FlatButton(
-              child: Text('Ok'),
-              onPressed: () {
-                Navigator.of(context).pop();
-              },
-            ),
-          ],
+        return Container(
+          child: AlertDialog(
+            shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
+            contentPadding: EdgeInsets.fromLTRB(10, 5, 10, 10),
+            elevation: 3.0,
+            title: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+              Icon(Icons.timer_off,
+              color: Color(0xff207FC5),),
+              Text('No time selected',
+              style: TextStyle(
+                color: Color(0xff207FC5)
+              ),)
+            ]),
+            actions: <Widget>[
+              FlatButton(
+                child: Text('Ok'),
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+              ),
+            ],
+          ),
         );
       },
     );
   }
+
 
   Future<Null> selectTime(BuildContext context) async {
     _time = await showTimePicker(
